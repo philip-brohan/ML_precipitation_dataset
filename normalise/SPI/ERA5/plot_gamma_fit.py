19b6c41e1bba313b4ba7c4e6952ca34806eef00f
Philip Brohan <philip@brohan.org>
2023-07-31 13:56:09 +0100

    Script to plot normalisation fit parameters.

--- normalise/SPI/ERA5/plot_gamma_fit.py
+++ normalise/SPI/ERA5/plot_gamma_fit.py
@@ -5,6 +5,7 @@
 
 import os
 import iris
+import numpy as np
 
 from utilities import plots
 from get_data.ERA5 import ERA5_monthly
@@ -19,14 +20,16 @@ import cmocean
 # Load the fitted values
 shape = iris.load_cube("%s/MLP/normalisation/SPI/ERA5/shape.nc" % os.getenv("SCRATCH"))
 ERA5_monthly.add_coord_system(shape)
-location = iris.load_cube("%s/MLP/normalisation/SPI/ERA5/location.nc" % os.getenv("SCRATCH"))
+location = iris.load_cube(
+    "%s/MLP/normalisation/SPI/ERA5/location.nc" % os.getenv("SCRATCH")
+)
 ERA5_monthly.add_coord_system(location)
 scale = iris.load_cube("%s/MLP/normalisation/SPI/ERA5/scale.nc" % os.getenv("SCRATCH"))
 ERA5_monthly.add_coord_system(scale)
 
 # Make the plot
 fig = Figure(
-    figsize=(10*3/2, 10),
+    figsize=(10 * 3 / 2, 10),
     dpi=100,
     facecolor=(0.5, 0.5, 0.5, 1),
     edgecolor=None,
@@ -57,12 +60,33 @@ axb.add_patch(
 )
 
 ax_shape = fig.add_axes([0.05, 0.68, 0.9, 0.31])
-plots.plotFieldAxes(ax_shape,shape,plotCube=plots.plot_cube(),cMap = cmocean.cm.thermal)
+plots.plotFieldAxes(
+    ax_shape,
+    shape,
+    plotCube=plots.plot_cube(),
+    vMin=np.percentile(shape.data.data, 5),
+    vMax=np.percentile(shape.data.data, 95),
+    cMap=cmocean.cm.rain,
+)
 
 ax_location = fig.add_axes([0.05, 0.34, 0.9, 0.31])
-plots.plotFieldAxes(ax_location,location,plotCube=plots.plot_cube(),cMap = cmocean.cm.thermal)
+plots.plotFieldAxes(
+    ax_location,
+    location,
+    plotCube=plots.plot_cube(),
+    vMin=np.percentile(location.data.data, 5),
+    vMax=np.percentile(location.data.data, 95),
+    cMap=cmocean.cm.rain,
+)
 
 ax_scale = fig.add_axes([0.05, 0.01, 0.9, 0.31])
-plots.plotFieldAxes(ax_scale,scale,plotCube=plots.plot_cube(),cMap = cmocean.cm.thermal)
+plots.plotFieldAxes(
+    ax_scale,
+    scale,
+    plotCube=plots.plot_cube(),
+    vMin=np.percentile(scale.data.data, 5),
+    vMax=np.percentile(scale.data.data, 95),
+    cMap=cmocean.cm.rain,
+)
 
 fig.savefig("gamma.png")
