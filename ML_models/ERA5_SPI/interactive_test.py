f3c7294c7692811f7cd56e77fc44e415bc29d64c
Philip Brohan <philip@brohan.org>
2023-08-30 11:39:22 +0100

    Matched lat::lon flip in model.

--- ML_models/ERA5_SPI/interactive_test.py
+++ ML_models/ERA5_SPI/interactive_test.py
@@ -3,7 +3,7 @@
 # To use a convnet on an arbitrarily-shaped tensor needs careful
 #  attention to padding to make the sizes come out correctly.
 # This is a test script - applying convolution layers to
-#  a test tensor to see what sizes the outputs are, so I can 
+#  a test tensor to see what sizes the outputs are, so I can
 #  tweak the padding params to get it right.
 # I ought to be able to work out the conv layer effects from
 #  the documentation, but I haven't managed it.
@@ -11,82 +11,72 @@
 import tensorflow as tf
 
 # Tensor of desired size to start with
-imt = tf.zeros(shape=([1,1440,721,1]))
+imt = tf.zeros(shape=([1, 721, 1440, 1]))
 
-print(' ')
+print(" ")
 tf.print(tf.shape(imt))
 
 step = tf.keras.layers.Conv2D(
     filters=5,
     kernel_size=3,
     strides=(2, 2),
-    padding="same",)(imt)
+    padding="same",
+)(imt)
 tf.print(tf.shape(step))
 step = tf.keras.layers.Conv2D(
     filters=10,
     kernel_size=3,
     strides=(2, 2),
-    padding="same",)(step)
+    padding="same",
+)(step)
 tf.print(tf.shape(step))
 step = tf.keras.layers.Conv2D(
     filters=10,
     kernel_size=3,
     strides=(2, 2),
-    padding="same",)(step)
+    padding="same",
+)(step)
 tf.print(tf.shape(step))
 step = tf.keras.layers.Conv2D(
     filters=20,
     kernel_size=3,
     strides=(2, 2),
-    padding="same",)(step)
+    padding="same",
+)(step)
 tf.print(tf.shape(step))
 step = tf.keras.layers.Conv2D(
     filters=40,
     kernel_size=3,
     strides=(2, 2),
-    padding="same",)(step)
+    padding="same",
+)(step)
 tf.print(tf.shape(step))
 step = tf.keras.layers.Flatten()(step)
 tf.print(tf.shape(step))
 step = tf.keras.layers.Dense(200)(step)
 tf.print(tf.shape(step))
-print(' ')
-step = tf.keras.layers.Dense(45*23*40)(step)
+print(" ")
+step = tf.keras.layers.Dense(23 * 45 * 40)(step)
 tf.print(tf.shape(step))
-step = tf.keras.layers.Reshape(target_shape=(45, 23, 40))(step)
+step = tf.keras.layers.Reshape(target_shape=(23, 45, 40))(step)
 tf.print(tf.shape(step))
-step=tf.keras.layers.Conv2DTranspose(
-    filters=20,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    output_padding=(1, 1))(step)
+step = tf.keras.layers.Conv2DTranspose(
+    filters=20, kernel_size=3, strides=2, padding="same", output_padding=(1, 1)
+)(step)
 tf.print(tf.shape(step))
-step=tf.keras.layers.Conv2DTranspose(
-    filters=10,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    output_padding=(1, 0))(step)
+step = tf.keras.layers.Conv2DTranspose(
+    filters=10, kernel_size=3, strides=2, padding="same", output_padding=(0, 1)
+)(step)
 tf.print(tf.shape(step))
-step=tf.keras.layers.Conv2DTranspose(
-    filters=10,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    output_padding=(1, 0))(step)
+step = tf.keras.layers.Conv2DTranspose(
+    filters=10, kernel_size=3, strides=2, padding="same", output_padding=(0, 1)
+)(step)
 tf.print(tf.shape(step))
-step=tf.keras.layers.Conv2DTranspose(
-    filters=5,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    output_padding=(1, 0))(step)
+step = tf.keras.layers.Conv2DTranspose(
+    filters=5, kernel_size=3, strides=2, padding="same", output_padding=(0, 1)
+)(step)
 tf.print(tf.shape(step))
-step=tf.keras.layers.Conv2DTranspose(
-    filters=1,
-    kernel_size=3,
-    strides=2,
-    padding="same",
-    output_padding=(1, 0))(step)
+step = tf.keras.layers.Conv2DTranspose(
+    filters=1, kernel_size=3, strides=2, padding="same", output_padding=(0, 1)
+)(step)
 tf.print(tf.shape(step))
