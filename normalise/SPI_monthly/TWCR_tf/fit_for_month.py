4fa83e6f1527a18598292c48e8b893d3f3997861
Philip Brohan <philip@brohan.org>
2023-10-02 17:09:05 +0100

    Split tf fitter script into data, model, and fit

--- normalise/SPI_monthly/TWCR_tf/fit_for_month.py
+++ normalise/SPI_monthly/TWCR_tf/fit_for_month.py
@@ -6,13 +6,9 @@ import os
 import sys
 import time
 import tensorflow as tf
-import tensorflow_probability as tfp
-import numpy as np
 
-from utilities import grids
-from get_data.TWCR import TWCR_monthly_load
-
-rng = np.random.default_rng()
+from makeDataset import getDataset
+from fitterModel import Gamma_Fitter
 
 import argparse
 
@@ -36,196 +32,11 @@ strategy = tf.distribute.MirroredStrategy()
 # How many epochs to train for
 nEpochs = 250
 nRepeatsPerEpoch = 1  # Show each month this many times
-bufferSize = 10# 00  # Already shuffled data, so not so important
-batchSize = 3# 2  # Arbitrary
+bufferSize = 10  # 00  # Already shuffled data, so not so important
+batchSize = 3  # 2  # Arbitrary
 
 # Start training rate
 training_rate = 0.1
-
-# Relative scaling factorsm for losses
-fit_scale = 1.0
-shape_regularization_factor=0.0
-scale_regularization_factor=0.0
-location_regularization_factor=0.0
-shape_neighbour_factor=0.0
-scale_neighbour_factor=0.0
-location_neighbour_factor=0.0
-
-
-# Load a pre-prepared tensor from a file
-def load_tensor(file_name):
-    sict = tf.io.read_file(file_name)
-    imt = tf.io.parse_tensor(sict, np.float32)
-    imt = tf.reshape(imt, [721, 1440, 1])
-    return imt
-
-
-# Get a list of filenames containing tensors
-def getFileNames(variable, month, startyear=1950, endyear=2014):
-    inFiles = sorted(
-        os.listdir(
-            "%s/MLP/normalisation/datasets/raw/%s" % (os.getenv("SCRATCH"), variable)
-        )
-    )
-    inFiles = [
-        fn
-        for fn in inFiles
-        if (
-            int(fn[:4]) >= startyear
-            and int(fn[5:7]) == month
-            and int(fn[:4]) <= endyear
-        )
-    ]
-    return inFiles
-
-
-# Get a dataset - all the tensors for a given month and variable
-def getDataset(variable, month, startyear=1950, endyear=2014, cache=False):
-    # Get a list of years to include
-    inFiles = getFileNames(variable, month, startyear=startyear, endyear=endyear)
-
-    # Create TensorFlow Dataset object from the source file names
-    tn_data = tf.data.Dataset.from_tensor_slices(tf.constant(inFiles))
-
-    # Convert from list of file names to Dataset of source file contents
-    fnFiles = [
-        "%s/MLP/normalisation/datasets/raw/%s/%s" % (os.getenv("SCRATCH"), variable, x)
-        for x in inFiles
-    ]
-    ts_data = tf.data.Dataset.from_tensor_slices(tf.constant(fnFiles))
-    ts_data = ts_data.map(load_tensor, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-    # Zip the data together with the years (so we can find the date and source of each
-    #   data tensor if we need it).
-    tz_data = tf.data.Dataset.zip((ts_data, tn_data))
-
-    # Optimisation
-    if cache:
-        tz_data = tz_data.cache()  # Great, iff you have enough RAM for it
-
-    tz_data = tz_data.prefetch(tf.data.experimental.AUTOTUNE)
-
-    return tz_data
-
-
-# Define a TF layer to compare its input against a gamma distribution
-# Returns the probability of the inputs given a gamma distribution
-#  defined by the layer weights. (Train weights to maximise this).
-class GammaC(tf.keras.layers.Layer):
-    def __init__(
-        self,
-    ):
-        super().__init__()
-
-    def build(self, input_shape):
-        self.shape = self.add_weight(
-            shape=input_shape[1:],
-            initializer=tf.keras.initializers.Constant(value=1.0),
-            trainable=True,
-            name='shape',
-        )
-        self.location = self.add_weight(
-            shape=input_shape[1:],
-            initializer=tf.keras.initializers.Constant(value=0.0),
-            trainable=True,
-            name='location',
-        )
-        self.scale = self.add_weight(
-            shape=input_shape[1:],
-            initializer=tf.keras.initializers.Constant(value=1.0),
-            trainable=True,
-            name='scale',
-        )
-
-    def call(self, inputs):
-        # Constrain scale and shape to be +ve
-        dists = tfp.distributions.Gamma(
-            concentration=tf.nn.relu(self.scale), rate=1.0 / tf.nn.relu(self.shape)
-        )
-        # Regularize
-        self.add_loss(
-            shape_regularization_factor * tf.reduce_mean(tf.square(self.shape))
-        )
-        self.add_loss(
-            scale_regularization_factor * tf.reduce_mean(tf.square(self.scale))
-        )
-        self.add_loss(
-            location_regularization_factor
-            * tf.reduce_mean(tf.square(self.location))
-        )
-        self.add_loss(
-            shape_neighbour_factor
-            * (
-                tf.reduce_mean(tf.square(self.shape[1:, :] - self.shape[:-1, :]))
-                + tf.reduce_mean(tf.square(self.shape[:, 1:] - self.shape[:, :-1]))
-            )
-        )
-        self.add_loss(
-            scale_neighbour_factor
-            * (
-                tf.reduce_mean(tf.square(self.scale[1:, :] - self.scale[:-1, :]))
-                + tf.reduce_mean(tf.square(self.scale[:, 1:] - self.scale[:, :-1]))
-            )
-        )
-        self.add_loss(
-            location_neighbour_factor
-            * (
-                tf.reduce_mean(tf.square(self.location[1:, :] - self.location[:-1, :]))
-                + tf.reduce_mean(
-                    tf.square(self.location[:, 1:] - self.location[:, :-1])
-                )
-            )
-        )
-        return dists.prob(inputs - self.location)
-
-
-# Define the model
-class Gamma_Fitter(tf.keras.Model):
-    # Initialiser - set up instance and define the models
-    def __init__(self):
-        super(Gamma_Fitter, self).__init__()
-
-        # Hyperparameters
-        # Max gradient to apply in optimizer
-        self.max_gradient = 2.0
-
-        # Model to encode input to latent space distribution
-        self.fitter = tf.keras.Sequential(
-            [tf.keras.layers.InputLayer(input_shape=(721, 1440, 1)), GammaC()]
-        )
-
-    def call(self, x, training=False):
-        probs = self.fitter(x, training=training)
-        return probs
-
-    @tf.function
-    def compute_loss(self, x, training):
-        fit_metric = tf.reduce_mean(tf.square(self.fitter(x[0], training=training)))
-        regularization_losses = self.losses
-        return tf.stack(
-            [
-            fit_metric,
-            regularization_losses[0],  # shape regularization
-            regularization_losses[1],  # scale regularization
-            regularization_losses[2],  # location regularization
-            regularization_losses[3],  # shape neighbours
-            regularization_losses[4],  # scale neighbours
-            regularization_losses[5],  # location neighbours
-            ]
-        )
-
-    # Run the fitter for one batch, calculate the errors, calculate the
-    #  gradients and update the layer weights.
-    @tf.function
-    def train_on_batch(self, x, optimizer):
-        with tf.GradientTape() as tape:
-            loss_values = self.compute_loss(x, training=True)
-            overall_loss = tf.math.reduce_sum(loss_values, axis=0)
-        gradients = tape.gradient(overall_loss, self.trainable_variables)
-        # Clip the gradients - helps against sudden numerical problems
-        gradients = [tf.clip_by_norm(g, self.max_gradient) for g in gradients]
-        optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-
 
 # Instantiate and run the fitter under the control of the distribution strategy
 with strategy.scope():
@@ -264,7 +75,7 @@ with strategy.scope():
     logfile_writer = tf.summary.create_file_writer(log_FN)
 
     # For each Epoch: train, save state, and report progress
-    for epoch in range(1,nEpochs + 1):
+    for epoch in range(1, nEpochs + 1):
         start_time = time.time()
 
         # Train on all batches in the training data
@@ -373,5 +184,10 @@ with strategy.scope():
             "Location : {:>9.3f} {:>9.3f}".format(
                 location_r_m.numpy() / batch_count,
                 location_n_m.numpy() / batch_count,
+            )
+        )
+        print(
+            "time: {} ".format(
+                int(end_training_time - start_time),
             )
         )
---
+++ normalise/SPI_monthly/TWCR_tf/fitterModel.py
@@ -0,0 +1,131 @@
+# Custom TF model for gamma distribution fitting
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+# Relative scaling factors for losses
+fit_scale = 1.0
+shape_regularization_factor = 0.0
+scale_regularization_factor = 0.0
+location_regularization_factor = 0.0
+shape_neighbour_factor = 0.0
+scale_neighbour_factor = 0.0
+location_neighbour_factor = 0.0
+
+
+# Define a TF layer to compare its input against a gamma distribution
+# Returns the probability of the inputs given a gamma distribution
+#  defined by the layer weights. (Train weights to maximise this).
+class GammaC(tf.keras.layers.Layer):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+
+    def build(self, input_shape):
+        self.shape = self.add_weight(
+            shape=input_shape[1:],
+            initializer=tf.keras.initializers.Constant(value=1.0),
+            trainable=True,
+            name="shape",
+        )
+        self.location = self.add_weight(
+            shape=input_shape[1:],
+            initializer=tf.keras.initializers.Constant(value=0.0),
+            trainable=True,
+            name="location",
+        )
+        self.scale = self.add_weight(
+            shape=input_shape[1:],
+            initializer=tf.keras.initializers.Constant(value=1.0),
+            trainable=True,
+            name="scale",
+        )
+
+    def call(self, inputs):
+        # Constrain scale and shape to be +ve
+        dists = tfp.distributions.Gamma(
+            concentration=tf.nn.relu(self.scale), rate=1.0 / tf.nn.relu(self.shape)
+        )
+        # Regularize
+        self.add_loss(
+            shape_regularization_factor * tf.reduce_mean(tf.square(self.shape))
+        )
+        self.add_loss(
+            scale_regularization_factor * tf.reduce_mean(tf.square(self.scale))
+        )
+        self.add_loss(
+            location_regularization_factor * tf.reduce_mean(tf.square(self.location))
+        )
+        self.add_loss(
+            shape_neighbour_factor
+            * (
+                tf.reduce_mean(tf.square(self.shape[1:, :] - self.shape[:-1, :]))
+                + tf.reduce_mean(tf.square(self.shape[:, 1:] - self.shape[:, :-1]))
+            )
+        )
+        self.add_loss(
+            scale_neighbour_factor
+            * (
+                tf.reduce_mean(tf.square(self.scale[1:, :] - self.scale[:-1, :]))
+                + tf.reduce_mean(tf.square(self.scale[:, 1:] - self.scale[:, :-1]))
+            )
+        )
+        self.add_loss(
+            location_neighbour_factor
+            * (
+                tf.reduce_mean(tf.square(self.location[1:, :] - self.location[:-1, :]))
+                + tf.reduce_mean(
+                    tf.square(self.location[:, 1:] - self.location[:, :-1])
+                )
+            )
+        )
+        return dists.prob(inputs - self.location)
+
+
+# Define the model
+class Gamma_Fitter(tf.keras.Model):
+    # Initialiser - set up instance and define the models
+    def __init__(self):
+        super(Gamma_Fitter, self).__init__()
+
+        # Hyperparameters
+        # Max gradient to apply in optimizer
+        self.max_gradient = 2.0
+
+        # Model to encode input to latent space distribution
+        self.fitter = tf.keras.Sequential(
+            [tf.keras.layers.InputLayer(input_shape=(721, 1440, 1)), GammaC()]
+        )
+
+    def call(self, x, training=False):
+        probs = self.fitter(x, training=training)
+        return probs
+
+    @tf.function
+    def compute_loss(self, x, training):
+        fit_metric = tf.reduce_mean(tf.square(self.fitter(x[0], training=training)))
+        regularization_losses = self.losses
+        return tf.stack(
+            [
+                fit_metric,
+                regularization_losses[0],  # shape regularization
+                regularization_losses[1],  # scale regularization
+                regularization_losses[2],  # location regularization
+                regularization_losses[3],  # shape neighbours
+                regularization_losses[4],  # scale neighbours
+                regularization_losses[5],  # location neighbours
+            ]
+        )
+
+    # Run the fitter for one batch, calculate the errors, calculate the
+    #  gradients and update the layer weights.
+    @tf.function
+    def train_on_batch(self, x, optimizer):
+        with tf.GradientTape() as tape:
+            loss_values = self.compute_loss(x, training=True)
+            overall_loss = tf.math.reduce_sum(loss_values, axis=0)
+        gradients = tape.gradient(overall_loss, self.trainable_variables)
+        # Clip the gradients - helps against sudden numerical problems
+        gradients = [tf.clip_by_norm(g, self.max_gradient) for g in gradients]
+        optimizer.apply_gradients(zip(gradients, self.trainable_variables))
