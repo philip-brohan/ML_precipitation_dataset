91a77cf5648461141d2a4344406d5dd57b580d02
Philip Brohan <philip@brohan.org>
2023-11-21 20:19:26 +0000

    Make GPCC blended normalised tensors

---
+++ make_normalised_tensors/GPCC_tf_MM/blended/make_all_tensors.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Make normalised data tensors for analysis
+
+import os
+
+
+def is_done(year, month):
+    fn = "%s/MLP/normalised_datasets/GPCC_tf_MM/blended/%04d-%02d.tfd" % (
+        os.getenv("SCRATCH"),
+        year,
+        month,
+    )
+    if os.path.exists(fn):
+        return True
+    return False
+
+
+for year in range(1979, 2019 + 1):
+    for month in range(1, 13):
+        if is_done(year, month):
+            continue
+        cmd = "./make_training_tensor.py --year=%04d --month=%02d" % (
+            year,
+            month,
+        )
+        print(cmd)
