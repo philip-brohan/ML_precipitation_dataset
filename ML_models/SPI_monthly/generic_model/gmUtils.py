a07791374413d09b2988413f9354116ce5e5bd18
Philip Brohan <philip@brohan.org>
2024-01-11 14:48:32 +0000

    Added another validation plot to generic model.

--- ML_models/SPI_monthly/generic_model/gmUtils.py
+++ ML_models/SPI_monthly/generic_model/gmUtils.py
@@ -3,6 +3,9 @@
 import os
 import numpy as np
 
+import datetime
+import iris
+
 import tensorflow as tf
 from tensorflow.core.util import event_pb2
 from tensorflow.python.framework import tensor_util
@@ -333,4 +336,151 @@ def plotTrainingMetrics(
         addLine(ax_kld[1], chts, "Test_logqz_x", (0, 0, 1, 1), 20)
 
     # Output as png
+    fig.savefig(fileName)
+
+
+# Get target and encoded scalar statistics for one test case
+def computeScalarStats(
+    specification, model, x, min_lat=-90, max_lat=90, min_lon=-180, max_lon=180
+):
+    nFields = specification["nOutputChannels"]
+
+    # get the date from the filename tensor
+    dateStr = tf.strings.split(x[0][0][0], sep="/")[-1].numpy()
+    year = int(dateStr[:4])
+    month = int(dateStr[5:7])
+    dtp = datetime.date(year, month, 15)
+    # Pass the test field through the autoencoder
+    generated = model.call(x, training=False)
+
+    def tensor_to_cube(t):
+        result = grids.E5sCube.copy()
+        result.data = np.squeeze(t.numpy())
+        result.data = np.ma.masked_where(result.data == 0.0, result.data, copy=False)
+        return result
+
+    def field_to_scalar(field):
+        field = field.extract(
+            iris.Constraint(
+                coord_values={"grid_latitude": lambda cell: min_lat <= cell <= max_lat}
+            )
+            & iris.Constraint(
+                coord_values={"grid_longitude": lambda cell: min_lon <= cell <= max_lon}
+            )
+        )
+        return np.mean(field.data)
+
+    stats = {}
+    stats["dtp"] = dtp
+    stats["target"] = {}
+    stats["generated"] = {}
+    for varI in range(nFields):
+        stats["target"][specification["outputNames"][varI]] = field_to_scalar(
+            tensor_to_cube(tf.squeeze(x[-1][:, :, :, varI])),
+        )
+        stats["generated"][specification["outputNames"][varI]] = field_to_scalar(
+            tensor_to_cube(tf.squeeze(generated[:, :, :, varI])),
+        )
+    return stats
+
+
+def plotScalarStats(all_stats, specification, fileName="multi.webp"):
+    nFields = specification["nOutputChannels"]
+
+    figScale = 3.0
+    wRatios = (3, 1.25)
+
+    # Make the plot
+    fig = Figure(
+        figsize=(figScale * sum(wRatios), figScale * nFields),
+        dpi=300,
+        facecolor=(0.5, 0.5, 0.5, 1),
+        edgecolor=None,
+        linewidth=0.0,
+        frameon=False,
+        subplotpars=None,
+        tight_layout=None,
+    )
+    canvas = FigureCanvas(fig)
+    font = {
+        "family": "sans-serif",
+        "sans-serif": "Arial",
+        "weight": "normal",
+        "size": 14,
+    }
+    matplotlib.rc("font", **font)
+    axb = fig.add_axes([0, 0, 1, 1])
+    axb.add_patch(
+        Rectangle(
+            (0, 0),
+            1,
+            1,
+            facecolor=(0.95, 0.95, 0.95, 1),
+            fill=True,
+            zorder=1,
+        )
+    )
+
+    # Plot a variable in its subfigure
+    def plot_var(sfig, ts, t, m, label):
+        # Get two axes in the subfig
+        var_axes = sfig.subplots(nrows=1, ncols=2, width_ratios=wRatios, squeeze=False)
+
+        # Calculate y range
+        ymin = min(min(t), min(m))
+        ymax = max(max(t), max(m))
+        ypad = (ymax - ymin) * 0.1
+        if ypad == 0:
+            ypad = 1
+
+        # First subaxis for time-series plot
+        var_axes[0, 0].set_xlim(
+            ts[0] - datetime.timedelta(days=15), ts[-1] + datetime.timedelta(days=15)
+        )
+        var_axes[0, 0].set_ylim(ymin - ypad, ymax + ypad)
+        var_axes[0, 0].grid(color=(0, 0, 0, 1), linestyle="-", linewidth=0.1)
+        var_axes[0, 0].text(
+            ts[0] - datetime.timedelta(days=15),
+            ymax + ypad,
+            label,
+            ha="left",
+            va="top",
+            bbox=dict(boxstyle="square,pad=0.5", fc=(1, 1, 1, 1)),
+            zorder=100,
+        )
+        var_axes[0, 0].add_line(
+            Line2D(ts, t, linewidth=2, color=(0, 0, 0, 1), alpha=1.0, zorder=50)
+        )
+        var_axes[0, 0].add_line(
+            Line2D(ts, m, linewidth=2, color=(1, 0, 0, 1), alpha=1.0, zorder=60)
+        )
+
+        # Second subaxis for scatter plot
+        var_axes[0, 1].set_xlim(ymin - ypad, ymax + ypad),
+        var_axes[0, 1].set_ylim(ymin - ypad, ymax + ypad),
+        var_axes[0, 1].grid(color=(0, 0, 0, 1), linestyle="-", linewidth=0.1)
+        var_axes[0, 1].scatter(t, m, s=2, color=(1, 0, 0, 1), zorder=60)
+        var_axes[0, 1].add_line(
+            Line2D(
+                (ymin - ypad, ymax + ypad),
+                (ymin - ypad, ymax + ypad),
+                linewidth=1,
+                color=(0, 0, 0, 1),
+                alpha=0.2,
+                zorder=10,
+            )
+        )
+
+    # Each variable in its own subfig
+    subfigs = fig.subfigures(nFields, 1, wspace=0.01)
+    for varI in range(nFields):
+        vName = specification["outputNames"][varI]
+        plot_var(
+            subfigs[varI],
+            all_stats["dtp"],
+            all_stats["target"][vName],
+            all_stats["generated"][vName],
+            vName,
+        )
+
     fig.savefig(fileName)
--- ML_models/SPI_monthly/generic_model/validate_multi.py
+++ ML_models/SPI_monthly/generic_model/validate_multi.py
@@ -2,21 +2,6 @@
 
 # Plot validation statistics for all the test cases
 
-import os
-import sys
-import numpy as np
-import tensorflow as tf
-import iris
-import iris.fileformats
-import iris.analysis
-import datetime
-
-import matplotlib
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-from matplotlib.patches import Rectangle
-from matplotlib.lines import Line2D
-
 from specify import specification
 
 # I don't need all the messages about a missing font
@@ -76,6 +61,10 @@ from utilities import grids
 
 from ML_models.SPI_monthly.generic_model.makeDataset import getDataset
 from ML_models.SPI_monthly.generic_model.autoencoderModel import DCVAE, getModel
+from ML_models.SPI_monthly.generic_model.gmUtils import (
+    computeScalarStats,
+    plotScalarStats,
+)
 
 # Set up the test data
 purpose = "Test"
@@ -84,65 +73,24 @@ if args.training:
 dataset = getDataset(specification, purpose=purpose)
 dataset = dataset.batch(1)
 
+# Load the trained model
 autoencoder = getModel(specification, args.epoch)
 
-
-def tensor_to_cube(t):
-    result = grids.E5sCube.copy()
-    result.data = np.squeeze(t.numpy())
-    result.data = np.ma.masked_where(result.data == 0.0, result.data, copy=False)
-    return result
-
-
-def field_to_scalar(field):
-    field = field.extract(
-        iris.Constraint(
-            coord_values={
-                "grid_latitude": lambda cell: args.min_lat <= cell <= args.max_lat
-            }
-        )
-        & iris.Constraint(
-            coord_values={
-                "grid_longitude": lambda cell: args.min_lon <= cell <= args.max_lon
-            }
-        )
-    )
-    return np.mean(field.data)
-
-
-nFields = specification["nOutputChannels"]
-
-
-# Get target and encoded statistics for one test case
-def compute_stats(model, x):
-    # get the date from the filename tensor
-    dateStr = tf.strings.split(x[0][0][0], sep="/")[-1].numpy()
-    year = int(dateStr[:4])
-    month = int(dateStr[5:7])
-    dtp = datetime.date(year, month, 15)
-    # Pass the test field through the autoencoder
-    generated = model.call(x, training=False)
-
-    stats = {}
-    stats["dtp"] = dtp
-    stats["target"] = {}
-    stats["generated"] = {}
-    for varI in range(nFields):
-        stats["target"][specification["outputNames"][varI]] = field_to_scalar(
-            tensor_to_cube(tf.squeeze(x[-1][:, :, :, varI])),
-        )
-        stats["generated"][specification["outputNames"][varI]] = field_to_scalar(
-            tensor_to_cube(tf.squeeze(generated[:, :, :, varI])),
-        )
-    return stats
-
-
+# Go through the data and get the scalar stat for each test month
 all_stats = {}
 all_stats["dtp"] = []
 all_stats["target"] = {}
 all_stats["generated"] = {}
 for case in dataset:
-    stats = compute_stats(autoencoder, case)
+    stats = computeScalarStats(
+        specification,
+        autoencoder,
+        case,
+        min_lat=args.min_lat,
+        max_lat=args.max_lat,
+        min_lon=args.min_lon,
+        max_lon=args.max_lon,
+    )
     all_stats["dtp"].append(stats["dtp"])
     for key in stats["target"].keys():
         if key in all_stats["target"]:
@@ -152,104 +100,5 @@ for case in dataset:
             all_stats["target"][key] = [stats["target"][key]]
             all_stats["generated"][key] = [stats["generated"][key]]
 
-
-figScale = 3.0
-wRatios = (3, 1.25)
-
 # Make the plot
-fig = Figure(
-    figsize=(figScale * sum(wRatios), figScale * nFields),
-    dpi=300,
-    facecolor=(0.5, 0.5, 0.5, 1),
-    edgecolor=None,
-    linewidth=0.0,
-    frameon=False,
-    subplotpars=None,
-    tight_layout=None,
-)
-canvas = FigureCanvas(fig)
-font = {
-    "family": "sans-serif",
-    "sans-serif": "Arial",
-    "weight": "normal",
-    "size": 14,
-}
-matplotlib.rc("font", **font)
-axb = fig.add_axes([0, 0, 1, 1])
-axb.add_patch(
-    Rectangle(
-        (0, 0),
-        1,
-        1,
-        facecolor=(0.95, 0.95, 0.95, 1),
-        fill=True,
-        zorder=1,
-    )
-)
-
-
-# Plot a variable in its subfigure
-def plot_var(sfig, ts, t, m, label):
-    # Get two axes in the subfig
-    var_axes = sfig.subplots(nrows=1, ncols=2, width_ratios=wRatios, squeeze=False)
-
-    # Calculate y range
-    ymin = min(min(t), min(m))
-    ymax = max(max(t), max(m))
-    ypad = (ymax - ymin) * 0.1
-    if ypad == 0:
-        ypad = 1
-
-    # First subaxis for time-series plot
-    var_axes[0, 0].set_xlim(
-        ts[0] - datetime.timedelta(days=15), ts[-1] + datetime.timedelta(days=15)
-    )
-    var_axes[0, 0].set_ylim(ymin - ypad, ymax + ypad)
-    var_axes[0, 0].grid(color=(0, 0, 0, 1), linestyle="-", linewidth=0.1)
-    var_axes[0, 0].text(
-        ts[0] - datetime.timedelta(days=15),
-        ymax + ypad,
-        label,
-        ha="left",
-        va="top",
-        bbox=dict(boxstyle="square,pad=0.5", fc=(1, 1, 1, 1)),
-        zorder=100,
-    )
-    var_axes[0, 0].add_line(
-        Line2D(ts, t, linewidth=2, color=(0, 0, 0, 1), alpha=1.0, zorder=50)
-    )
-    var_axes[0, 0].add_line(
-        Line2D(ts, m, linewidth=2, color=(1, 0, 0, 1), alpha=1.0, zorder=60)
-    )
-
-    # Second subaxis for scatter plot
-    var_axes[0, 1].set_xlim(ymin - ypad, ymax + ypad),
-    var_axes[0, 1].set_ylim(ymin - ypad, ymax + ypad),
-    var_axes[0, 1].grid(color=(0, 0, 0, 1), linestyle="-", linewidth=0.1)
-    var_axes[0, 1].scatter(t, m, s=2, color=(1, 0, 0, 1), zorder=60)
-    var_axes[0, 1].add_line(
-        Line2D(
-            (ymin - ypad, ymax + ypad),
-            (ymin - ypad, ymax + ypad),
-            linewidth=1,
-            color=(0, 0, 0, 1),
-            alpha=0.2,
-            zorder=10,
-        )
-    )
-
-
-# Each variable in its own subfig
-subfigs = fig.subfigures(nFields, 1, wspace=0.01)
-for varI in range(nFields):
-    vName = specification["outputNames"][varI]
-    plot_var(
-        subfigs[varI],
-        all_stats["dtp"],
-        all_stats["target"][vName],
-        all_stats["generated"][vName],
-        vName,
-    )
-
-
-fig.savefig("multi.png")
+plotScalarStats(all_stats, specification, fileName="multi.webp")
