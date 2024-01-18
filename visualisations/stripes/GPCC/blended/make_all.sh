2af689dfd208ad9f16fa7471ab2b3eee394f5464
Philip Brohan <philip@brohan.org>
2024-01-18 17:02:04 +0000

    Typo fix

--- visualisations/stripes/GPCC/blended/make_all.sh
+++ visualisations/stripes/GPCC/blended/make_all.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "SCRIPT_DIR/stripes.py --convolve=none --startyear=1850"
-echo "SCRIPT_DIR/stripes.py --convolve=11x13 --startyear=1850 --vmin=0.25 --vmax=0.75"
-echo "SCRIPT_DIR/stripes.py --convolve=sub-11x13 --startyear=1850"
-echo "SCRIPT_DIR/stripes.py --reduce=mean --startyear=1850"
+echo "$SCRIPT_DIR/stripes.py --convolve=none --startyear=1850"
+echo "$SCRIPT_DIR/stripes.py --convolve=11x13 --startyear=1850 --vmin=0.25 --vmax=0.75"
+echo "$SCRIPT_DIR/stripes.py --convolve=sub-11x13 --startyear=1850"
+echo "$SCRIPT_DIR/stripes.py --reduce=mean --startyear=1850"
