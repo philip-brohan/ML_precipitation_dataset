bd79fb695a85a803987be015a496206b08af7740
Philip Brohan <philip@brohan.org>
2024-01-18 13:11:19 +0000

    Re-include HadISST in list of datasets.

--- normalise/SPI_monthly/normalise_all.sh
+++ normalise/SPI_monthly/normalise_all.sh
@@ -11,8 +11,8 @@
 (cd ./GPCC_tf_MM/in_situ && ./make_all_fits.py)
 (cd ./GPCC_tf_MM/blended && ./make_all_fits.py)
 
-#(cd HadCRUT_tf_MM && ./make_all_fits.py)
+(cd HadCRUT_tf_MM && ./make_all_fits.py)
 
-#(cd HadISST_tf_MM && ./make_all_fits.py)
+(cd HadISST_tf_MM && ./make_all_fits.py)
 
 (cd GPCP_tf_MM && ./make_all_fits.py)
