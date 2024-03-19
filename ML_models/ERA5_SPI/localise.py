c7a7e3b8a5834e13789aedd08c264f44aa5f3024
Philip Brohan <philip@brohan.org>
2024-03-19 10:49:25 +0000

    Deleted old model.

--- ML_models/ERA5_SPI/localise.py
+++
@@ -1,8 +0,0 @@
-# Define a variable with the name of this model
-
-# This name is used in all the model IO directories
-
-# This means that I can make a variant model by copying the whole directory
-#  and just changing the name in this file.
-
-ModelName = "ERA5_SPI"
