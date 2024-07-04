f259b0b25ee047d8454ac52954e20d2e55bf3f10
Philip Brohan <philip@brohan.org>
2024-07-04 10:42:06 +0100

    Add OCADA to normalize_all script

--- normalize/SPI_monthly/normalize_all.sh
+++ normalize/SPI_monthly/normalize_all.sh
@@ -9,6 +9,8 @@
 
 (cd TWCR_tf_MM && ./make_all_fits.py)
 
+(cd OCADA_tf_MM && ./make_all_fits.py)
+
 (cd ./GPCC_tf_MM/in_situ && ./make_all_fits.py)
 (cd ./GPCC_tf_MM/blended && ./make_all_fits.py)
 
