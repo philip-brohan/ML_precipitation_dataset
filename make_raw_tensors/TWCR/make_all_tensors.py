c0bda12f7137cae54a7f9523531a735d746cb658
Philip Brohan <philip@brohan.org>
2024-07-16 13:45:21 +0100

    TWCR raw tensors to zarr

--- make_raw_tensors/TWCR/make_all_tensors.py
+++ make_raw_tensors/TWCR/make_all_tensors.py
@@ -3,7 +3,13 @@
 # Make raw data tensors for normalisation
 
 import os
+import sys
 import argparse
+import zarr
+import tensorstore as ts
+import numpy as np
+
+from tensor_utils import date_to_index, FirstYear, LastYear
 
 sDir = os.path.dirname(os.path.realpath(__file__))
 
@@ -17,32 +23,53 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def is_done(year, month, variable, member):
-    fn = "%s/MLP/raw_datasets/TWCR/%s/%02d/%04d-%02d.tfd" % (
-        os.getenv("SCRATCH"),
-        variable,
-        member,
-        year,
-        month,
-    )
-    if os.path.exists(fn):
-        return True
-    return False
+# Output zarr array location
+fn = "%s/MLP/raw_datasets/TWCR/%s_zarr" % (
+    os.getenv("SCRATCH"),
+    args.variable,
+)
 
+# Create TensorStore dataset if it doesn't exist
+try:
+    dataset = ts.open(
+        {
+            "driver": "zarr",
+            "kvstore": "file://" + fn,
+        },
+        dtype=ts.float32,
+        chunk_layout=ts.ChunkLayout(chunk_shape=[721, 1440, 80, 1]),
+        create=True,
+        fill_value=np.nan,
+        shape=[
+            721,
+            1440,
+            80,
+            date_to_index(LastYear, 12) + 1,
+        ],
+    ).result()
+except ValueError as e:  # Already exists
+    pass
 
-for year in range(1850, 2014):
-    for month in range(1, 13):
-        for member in range(1, 81):
-            if is_done(year, month, args.variable, member):
-                continue
-            cmd = (
-                "%s/make_training_tensor.py --year=%04d --month=%02d --variable=%s --member=%d"
-                % (
-                    sDir,
-                    year,
-                    month,
-                    args.variable,
-                    member,
+# Add date range to array as metadata
+# TensorStore doesn't support metadata, so use the underlying zarr array
+zarr_ds = zarr.open(fn, mode="r+")
+zarr_ds.attrs["FirstYear"] = FirstYear
+zarr_ds.attrs["LastYear"] = LastYear
+
+for member in range(1, 81):
+    for year in range(FirstYear, FirstYear + 10):
+        for month in range(1, 13):
+            idx = date_to_index(year, month)
+            slice = zarr_ds[:, :, member - 1, idx]
+            if np.all(np.isnan(slice)):  # Data missing, so make it
+                cmd = (
+                    "%s/make_training_tensor.py --year=%04d --month=%02d --variable=%s --member=%d"
+                    % (
+                        sDir,
+                        year,
+                        month,
+                        args.variable,
+                        member,
+                    )
                 )
-            )
-            print(cmd)
+                print(cmd)
--- make_raw_tensors/TWCR/make_training_tensor.py
+++ make_raw_tensors/TWCR/make_training_tensor.py
@@ -6,14 +6,23 @@
 
 import os
 import sys
+import numpy as np
+import warnings
+
+# Supress TensorFlow moaning about cuda - we don't need a GPU for this
+# Also the warning message confuses people.
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
 import tensorflow as tf
+import tensorstore as ts
+
 import dask
 
 # Going to do external parallelism - run this on one core
 tf.config.threading.set_inter_op_parallelism_threads(1)
 dask.config.set(scheduler="single-threaded")
 
-from tensor_utils import load_raw, raw_to_tensor
+from tensor_utils import load_raw, raw_to_tensor, date_to_index
 
 import argparse
 
@@ -22,26 +31,32 @@ parser.add_argument("--year", help="Year", type=int, required=True)
 parser.add_argument("--month", help="Integer month", type=int, required=True)
 parser.add_argument("--member", help="Ensemble member", type=int, required=True)
 parser.add_argument("--variable", help="Variable name", type=str, required=True)
-parser.add_argument(
-    "--opfile", help="tf data file name", default=None, type=str, required=False
-)
 args = parser.parse_args()
-if args.opfile is None:
-    args.opfile = ("%s/MLP/raw_datasets/TWCR/%s/%02d/%04d-%02d.tfd") % (
-        os.getenv("SCRATCH"),
-        args.variable,
-        args.member,
-        args.year,
-        args.month,
-    )
 
-if not os.path.isdir(os.path.dirname(args.opfile)):
-    os.makedirs(os.path.dirname(args.opfile))
+fn = "%s/MLP/raw_datasets/TWCR/%s_zarr" % (
+    os.getenv("SCRATCH"),
+    args.variable,
+)
+
+dataset = ts.open(
+    {
+        "driver": "zarr",
+        "kvstore": "file://" + fn,
+    }
+).result()
 
 # Load and standardise data
-qd = load_raw(args.year, args.month, member=args.member, variable=args.variable)
-ict = raw_to_tensor(qd)
+try:
+    qd = load_raw(args.year, args.month, member=args.member, variable=args.variable)
+    ict = raw_to_tensor(qd)
+except Exception:
+    warnings.warn(
+        "Failed to load data for %s %04d-%02d" % (args.variable, args.year, args.month)
+    )
+    ict = tf.fill([721, 1440], tf.constant(np.nan, dtype=tf.float32))
 
 # Write to file
-sict = tf.io.serialize_tensor(ict)
-tf.io.write_file(args.opfile, sict)
+didx = date_to_index(args.year, args.month)
+midx = args.member - 1
+op = dataset[:, :, midx, didx].write(ict)
+op.result()  # Ensure write completes before exiting
