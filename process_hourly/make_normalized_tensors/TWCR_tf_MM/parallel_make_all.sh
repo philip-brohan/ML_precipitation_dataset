ceea7302f9dcea1d5abc02c7228c4bdbfd4a17e2
Philip Brohan <philip@brohan.org>
2025-03-12 10:38:06 +0000

    More efficient parallelisation of the input pipeline.

--- process_hourly/make_normalized_tensors/TWCR_tf_MM/parallel_make_all.sh
+++ process_hourly/make_normalized_tensors/TWCR_tf_MM/parallel_make_all.sh
@@ -5,14 +5,17 @@ echo -n '' > mnth.txt
 for var in PRMSL TMP2m PRATE SST
 do
 
-# Initial minimal make to create the zarr structure (hence --delete)
-./make_all_tensors.py --variable=$var --month=3 --day=12 --hour=6 --delete
+    # Initial minimal make to create the zarr structure (hence --delete)
+    ./make_all_tensors.py --variable=$var --month=3 --day=12 --hour=6 --delete
 
-# Then parallelize by months
-for month in {1..12}
-do
-    echo "./make_all_tensors.py --variable=$var --month=$month" >> mnth.txt
-done
+    # Then parallelize by months
+    for month in {1..12}
+    do
+        for hour in $(seq 0 3 24)
+        do
+            echo "./make_all_tensors.py --variable=$var --month=$month --hour=$hour" >> mnth.txt
+        done
+    done
 
 done 
 
