c4fb3a94ff9cabfc8fd08cba3be2913bf5e13177
Philip Brohan <philip@brohan.org>
2025-05-12 10:23:10 +0100

    Make default hourly model.

---
+++ process_hourly/make_raw_tensors/TWCR/check_tensors.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# Check the raw tensor zarr array
+
+import os
+import argparse
+import zarr
+import numpy as np
+import datetime
+from get_data_hourly.TWCR import TWCR_hourly_load
+
+from tensor_utils import date_to_index
+
+sDir = os.path.dirname(os.path.realpath(__file__))
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--variable",
+    help="Variable name",
+    type=str,
+    required=True,
+)
+parser.add_argument(
+    "--month",
+    help="Restrict to selected month",
+    type=int,
+    required=False,
+    default=None,
+)
+parser.add_argument(
+    "--day",
+    help="Restrict to selected day of month",
+    type=int,
+    required=False,
+    default=None,
+)
+parser.add_argument(
+    "--hour",
+    help="Restrict to selected hour of day",
+    type=int,
+    required=False,
+    default=None,
+)
+args = parser.parse_args()
+
+# Find the raw_tensor zarr array
+fn = "%s/MLP/raw_datasets_hourly/TWCR/%s_zarr" % (
+    os.getenv("SCRATCH"),
+    args.variable,
+)
+
+# Add date range to array as metadata
+zarr_ds = zarr.open(fn, mode="r+")
+
+count = 0
+bad = 0
+missing = 0
+for year in range(1961, 1990 + 1):
+    for month in range(1, 13):
+        if args.month is not None and month != args.month:
+            continue
+        for day in range(1, 32):
+            if args.day is not None and day != args.day:
+                continue
+            for hour in range(0, 24, 3):
+                if args.hour is not None and hour != args.hour:
+                    continue
+                try:
+                    dte = datetime.datetime(year, month, day, hour)
+                    idx = date_to_index(year, month, day, hour)
+                    for member_idx in range(len(TWCR_hourly_load.members)):
+                        count += 1
+                        try:
+                            slice = zarr_ds[:, :, member_idx, idx]
+                            if np.any(np.isnan(slice)):
+                                print(
+                                    "Bad data for %04d-%02d-%02d:%02d_%02d %d"
+                                    % (year, month, day, hour, member_idx, idx)
+                                )
+                                bad += 1
+                        except Exception as e:
+                            print(
+                                "Missing data for %04d-%02d-%02d:%02d_%02d %d"
+                                % (year, month, day, hour, member_idx, idx)
+                            )
+                            missing += 1
+                except ValueError as e:
+                    pass
+
+
+print(args.variable)
+print("Missing member-hours:", missing)
+print("Bad member-hours:", bad)
+print("Total member-hours:", count)
+print("\n")
---
+++ process_hourly/normalize/TWCR_tf_MM/check_fit_parameters.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+# Check the fits for the normalization parameters
+#  mising files and bad values
+
+import os
+import iris
+import iris.time
+
+iris.FUTURE.datum_support = True
+import numpy as np
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--month", help="Month to plot", type=int, required=False, default=None
+)
+parser.add_argument("--day", help="Day to plot", type=int, required=False, default=None)
+parser.add_argument(
+    "--hour", help="Hour to plot", type=int, required=False, default=None
+)
+parser.add_argument(
+    "--variable",
+    help="Name of variable to use (PRATE, TMP2m, PRMSL, ...)",
+    type=str,
+    default="PRATE",
+)
+args = parser.parse_args()
+
+for month in range(1, 13):
+    if args.month is not None and month != args.month:
+        continue
+    for day in range(1, 32):
+        if args.day is not None and day != args.day:
+            continue
+        if month == 2 and day > 28:
+            continue
+        for hour in range(0, 24, 3):
+            if args.hour is not None and hour != args.hour:
+                continue
+            try:
+                dte = datetime.datetime(1961, month, day, hour)
+            except Exception as e:  # Some months have fewer than 31 days
+                continue
+            try:
+                shape = iris.load_cube(
+                    "%s/MLP/normalization/SPI_hourly/TWCR_tf_MM/%s/shape_m%02d_d%02d_h%02d.nc"
+                    % (
+                        os.getenv("SCRATCH"),
+                        args.variable,
+                        month,
+                        day,
+                        hour,
+                    ),
+                )
+            except Exception as e:
+                print(
+                    "Missing shape file for %s %02d-%02d:%02d"
+                    % (args.variable, month, day, hour)
+                )
+                continue
+            if np.any(np.isnan(shape.data)):
+                print(
+                    "Bad shape file for %s %02d-%02d:%02d"
+                    % (args.variable, month, day, hour)
+                )
+            try:
+                location = iris.load_cube(
+                    "%s/MLP/normalization/SPI_hourly/TWCR_tf_MM/%s/location_m%02d_d%02d_h%02d.nc"
+                    % (
+                        os.getenv("SCRATCH"),
+                        args.variable,
+                        month,
+                        day,
+                        hour,
+                    ),
+                )
+            except Exception as e:
+                print(
+                    "Missing location file for %s %02d-%02d:%02d"
+                    % (args.variable, month, day, hour)
+                )
+                continue
+            if np.any(np.isnan(location.data)):
+                print(
+                    "Bad location file for %s %02d-%02d:%02d"
+                    % (args.variable, month, day, hour)
+                )
+            try:
+                scale = iris.load_cube(
+                    "%s/MLP/normalization/SPI_hourly/TWCR_tf_MM/%s/scale_m%02d_d%02d_h%02d.nc"
+                    % (
+                        os.getenv("SCRATCH"),
+                        args.variable,
+                        month,
+                        day,
+                        hour,
+                    ),
+                )
+            except Exception as e:
+                print(
+                    "Missing scale file for %s %02d-%02d:%02d"
+                    % (args.variable, month, day, hour)
+                )
+                continue
+            if np.any(np.isnan(scale.data)):
+                print(
+                    "Bad scale file for %s %02d-%02d:%02d"
+                    % (args.variable, month, day, hour)
+                )
