61b6a75a4c266d12434f1ba971c00f76b18d8e52
Philip Brohan <philip@brohan.org>
2025-10-30 13:21:36 +0000

    Load HadIST to new disc location.

--- get_data/HadISST/v1/get_data.sh
+++ get_data/HadISST/v1/get_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p $SCRATCH/HadISST/v1
-wget https://www.metoffice.gov.uk/hadobs/hadisst/data/HadISST_sst.nc.gz -O $SCRATCH/HadISST/v1/HadISST_sst.nc.gz
-gunzip $SCRATCH/HadISST/v1/HadISST_sst.nc.gz
+mkdir -p $PDIR/HadISST/v1
+wget https://www.metoffice.gov.uk/hadobs/hadisst/data/HadISST_sst.nc.gz -O $PDIR/HadISST/v1/HadISST_sst.nc.gz
+gunzip $PDIR/HadISST/v1/HadISST_sst.nc.gz
